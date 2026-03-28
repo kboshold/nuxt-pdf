16ced43b2df41fa7d86fd3a3c002f2911ed7a76e
Kevin Boshold <github@proxy.boshol.de>
2026-03-28 22:40:57 +0100

    refactor: update cleanup plugin for graceful pool-aware shutdown

--- src/runtime/server/plugins/cleanup.ts
+++ src/runtime/server/plugins/cleanup.ts
@@ -1,15 +1,18 @@
 import { defineNitroPlugin } from 'nitropack/runtime'
 import { closeBrowser } from '../browser'
+import { getPool } from '../composables/usePDF'
 import { getLogger } from '../logger'
 
 async function cleanup() {
-  getLogger().info('Cleanup: closing browser')
-  await closeBrowser()
+  getLogger().info('Shutdown: draining pool and closing browser')
+  await closeBrowser(getPool())
+  getLogger().info('Shutdown: browser closed')
 }
 
 async function cleanupAndExit() {
-  getLogger().info('Cleanup: closing browser (signal)')
-  await closeBrowser()
+  getLogger().info('Shutdown: draining pool and closing browser (signal)')
+  await closeBrowser(getPool())
+  getLogger().info('Shutdown: browser closed')
   process.exit(0)
 }
 
